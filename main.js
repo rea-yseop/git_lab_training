47b40b7c64fdf722918da381bf256bf3fa7c5d17
ea <rea@yseop.com>
2019-03-08 14:36:43 +0100

    feat: change the main content

--- main.js
+++ main.js
@@ -4,12 +4,13 @@ import 'src/file2'
 import 'src/file3'
 
 function main() {
-
-    console.log('We are in the main, are you happy ?');
+	
+	console.log('We are in the main, are you happy ?');
 	
 	console.log('first function', myFirstFunction());
 	
 	console.log('text:', anotherFunction());
 	
+	console.log('power number:', calculatePower(4, 5));
 	console.log('power number:', calculatePower(7, 2));
 }
